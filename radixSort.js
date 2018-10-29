f019ee4ad0c06ac94d8164b5b95ba38415411dcf
Casperfeng <casp.feng@gmail.com>
2018-10-29 11:45:01 +0100

    made the scale of radixSort, not yet added stable sorting

---
+++ radixSort.js
@@ -0,0 +1,8 @@
+'use strict';
+//Not implemented yet
+function radixSort(A, d) {
+    for (let i = 0; i < d; i++) {
+        // use a stable sort to sort array A on digit i
+        // stable sorts: insertion, bubble, bucket, merge, counting etc.
+    }
+}
