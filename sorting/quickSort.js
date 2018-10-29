3fbceda9184d19dd89ebd72edfd2a958099f1584
Casperfeng <casp.feng@gmail.com>
2018-10-29 18:12:29 +0100

    added quicksort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -3,13 +3,17 @@
 
 /**
  *
- * @param A
- * @param p
- * @param r
- * @returns {*}
+ * @param A is array to be sorted
+ * @param p is an index
+ * @param r is an index
+ * given that p <= r
  */
 function quickSort(A, p, r){
-
+    if (p < r){
+        q = partition(A, p, r);
+        quickSort(A, p, q-1);
+        quickSort(A, q+1, r);
+    }
 }
 
 /**
@@ -22,10 +26,10 @@ function quickSort(A, p, r){
  * 2. If i + 1 ≤ k ≤ j – 1, then A[k] > x
  * 3. If k == r, then A[k] = x
  *
- * @param A is the array to be sorted
+ * @param A is the array to bse sorted
  * @param p is an index
  * @param r is an index
- * @returns {*}
+ * @returns
  * given that p <= r
  */
 
@@ -33,11 +37,9 @@ function partition(A, p, r){
 
     //the selected pivot element, x
     let x = A[r];
-
-
     let i = p - 1;
-
     //loops through the sub array
+    // ≤x | ≥x | x
     for (let j = p; j < r; j++){
         if (A[j] <= x){
             i++;
@@ -47,5 +49,15 @@ function partition(A, p, r){
             A[i] = temp;
         }
     }
+    //exchanges A[i + 1] and A[r]
+    let temp = A[i + 1];
+    A[i + 1] = A[r];
+    A[r] = temp;
+    //the list should be
+    //≤x | x | ≥x
     return i + 1;
 }
+
+A = [1,9,3,4,6,22,2,1,9,2,4,11,29];
+quickSort(A, 0, A.length-1);
+console.log(A);
